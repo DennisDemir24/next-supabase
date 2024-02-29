c805ff3b1b3bf9b6e65140dab9f505f31a4bb427
Dennis Demir <dd222gc@student.lnu.se>
2024-02-29 15:43:19 +0100

    Adding CRUD actions to boilerplate

--- src/actions/todos.actions.ts
+++ src/actions/todos.actions.ts
@@ -8,3 +8,33 @@ export const getTodos = async ({userId, token}: {userId: string; token: string})
         .eq('user_id', userId)
     return todos
 }
+
+export const getTodoById = async ({userId, token, todoId}: {userId: string; token: string; todoId: string}) => {
+    const supabase = await supabaseClient(token)
+    const {data: todo} = await supabase
+        .from('todos')
+        .select('*')
+        .eq('user_id', userId)
+        .eq('id', todoId)
+    return todo
+}
+
+export const updateTodo = async ({userId, token, todoId, title, description}: {userId: string; token: string; todoId: string; title: string; description: string}) => {
+    const supabase = await supabaseClient(token)
+    const {data: todo} = await supabase
+        .from('todos')
+        .update({title, description})
+        .eq('user_id', userId)
+        .eq('id', todoId)
+    return todo
+}
+
+export const deleteTodo = async ({userId, token, todoId}: {userId: string; token: string; todoId: string}) => {
+    const supabase = await supabaseClient(token)
+    const {data: todo} = await supabase
+        .from('todos')
+        .delete()
+        .eq('user_id', userId)
+        .eq('id', todoId)
+    return todo
+}
